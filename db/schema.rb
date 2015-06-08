91a87cf8938aa75548299f3cce84d685c969b7ab
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-06-08 16:44:56 -0600

    fixes PG errors

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150608220953) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.datetime "sent"
     t.text     "content"
